3e6e733b6ef6e1f908e020c49aa38389b6e2ac82
Ramesh Baskarasubramanian <agile.testing.thoughts@gmail.com>
2011-09-14 07:08:10 -0400

    Adding the initial source folder. More packages and clases will be added subsequently.

---
+++ src/com/automation/core/Driver.java
@@ -0,0 +1,11 @@
+package com.automation.core;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: rameshb
+ * Date: 9/14/11
+ * Time: 7:07 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Driver {
+}
